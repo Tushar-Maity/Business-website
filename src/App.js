a07d10f2a262bdeaae308bcf343f676dea80ae83
Tushar_Maity <68024552+Tushar-Maity@users.noreply.github.com>
2021-06-04 00:52:36 +0530

    GlobalStyles added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
+import GlobalStyle from "./components/GlobalStyles"
 
 function App() {
   return (
     <div>
+       <GlobalStyle />
        <Navbar />
        Landing Page
     </div>
---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,12 @@
+import styled, { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'S';
+    }
+`
+
+export default GlobalStyle

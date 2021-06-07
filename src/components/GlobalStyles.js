4cb2893d003a961d5cdb953993c9755a7a88f6fe
Tushar_Maity <68024552+Tushar-Maity@users.noreply.github.com>
2021-06-07 16:52:07 +0530

    NavBar is done

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -21,4 +21,22 @@ export const StyledContainer = styled.div`
     }
 `
 
+export const StyledButton = styled.button`
+    border-radius: 4px;
+    white-space: nowrap;
+    background: ${({ primary }) => (primary ? '#4b59f7' : '#0467fb')};
+    padding: ${({ big }) => (big ? '12px 64px' : '10px 20px')};
+    font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
+    color: #fff;
+    cursor: pointer;
+    border: none;
+    outline: none;
+    transition: all .3s ease-out;
+
+    &:hover {
+        background: #fff;
+        background: ${({ primary }) => (primary ? '#0467fb' : '#4b59f7')};
+    }
+`
+
 export default GlobalStyle
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from "styled-components"
 import { FaBars, FaMagento, FaTimes } from 'react-icons/fa'
 import { IconContext } from 'react-icons/lib'
 import { Link } from 'react-router-dom'
-import { StyledContainer } from "./GlobalStyles"
+import { StyledContainer, StyledButton } from "./GlobalStyles"
 
 
 const Navbar = () => {
 
     const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true)
 
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        }
+        else {
+            setButton(true)
+        }
+    }
+
+    useEffect(() => {
+        showButton()
+    }, [])
+
+    window.addEventListener('resize', showButton)
     return (
         <IconContext.Provider value={{color: '#fff'}}>
             <StyledNav>
@@ -21,6 +36,41 @@ const Navbar = () => {
                     <StyledMobIcon onClick={() => setClick(!click)}>
                         {click ? <FaTimes /> : <FaBars />}
                     </StyledMobIcon>
+                    <StyledNavMenu click={click}>
+                        <StyledNavItem>
+                            <StyledNavLink to="/">
+                                Home
+                            </StyledNavLink>
+                        </StyledNavItem>
+
+                        <StyledNavItem>
+                            <StyledNavLink to="/services">
+                                Services
+                            </StyledNavLink>
+                        </StyledNavItem>
+
+                        <StyledNavItem>
+                            <StyledNavLink to="/products">
+                                Products
+                            </StyledNavLink>
+                        </StyledNavItem>
+
+                        <StyledNavBtn>
+                            { button ? (
+                                <StyledNavBtnLink to="/signup">
+                                    <StyledButton primary>
+                                        Sign Up
+                                    </StyledButton>
+                                </StyledNavBtnLink>
+                            ) : 
+                                <StyledNavBtnLink to="/signup">
+                                    <StyledButton primary fontBig>
+                                        Sign Up
+                                    </StyledButton>
+                                </StyledNavBtnLink>
+                                }
+                        </StyledNavBtn>
+                    </StyledNavMenu>
                 </StyledNavBar>
             </StyledNav>
         </IconContext.Provider>
@@ -71,6 +121,79 @@ const StyledMobIcon = styled.div`
         font-size: 1.8rem;
         cursor: pointer;
     }
+`
+
+const StyledNavMenu = styled.ul`
+    display: flex;
+    align-items: center;
+    list-style: none;
+    text-align: center;
+
+    @media screen and (max-width: 960px) {
+        flex-direction: column;
+        width: 100%;
+        height: 90vh;
+        position: absolute;
+        top: 80px;
+        left: ${({click}) => (click ? 0 : '-100%')};
+        opacity: 1;
+        background: #101522;
+        transition: all .5s ease;
+    }
+`
+const StyledNavItem = styled.li`
+    height: 80px;
+    border-bottom: 4px solid transparent;
+
+     &:hover {
+         border-bottom: 4px solid #4b59f7;
+     }
+
+     @media screen and (max-width: 960px) {
+        width: 100%;
+        &:hover {
+            border: none;
+        }
+    }
+`
+const StyledNavLink = styled(Link)`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    height: 100%;
+    padding: .5rem 1rem;
+
+    @media screen and (max-width: 960px) {
+        text-align: center;
+        padding: 2rem;
+        width: 100%;
+        display: table;
+
+        &:hover {
+            color: #4b59f7;
+            transition: all .3s ease;
+        }
+    }
+`
+
+const StyledNavBtn = styled.li`
+    @media screen and (max-width: 960px) {
+        display: flex;
+        justify-content: center;
+        align-center: center;
+        height: 120px;
+    }
+`
+
+const StyledNavBtnLink = styled(Link)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    padding: 8px 16px;
+    height: 100%;
+    width: 100%
 `
 
 export default Navbar
